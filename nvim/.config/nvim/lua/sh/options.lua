c970fb4e4e89f86f7a043c10b99b9e02894138c6
Sudhir Hanumanthappa <sudhih@gmail.com>
2022-07-24 06:12:18 +0530

    same settings, but different way of implementation using for loop

--- nvim/.config/nvim/lua/sh/options.lua
+++ nvim/.config/nvim/lua/sh/options.lua
@@ -1,39 +1,56 @@
--- :help options :help vim.opt
-local o=vim.opt
+local options = { 
+    -- about opened files
+    backup = false,                         -- creates a backup file
+    swapfile = false,                       -- don't create swap files
+    clipboard = "unnamed",                  -- allow neovim to aceess system clipboard
+    conceallevel = 0,                       -- so that `` is visible in markdown files
+    fileencoding = "utf-8",                 -- the encoding written to a file
+    undofile = true,                        -- enable persistent undo
+    writebackup = false,                    -- if a file is being edited by another program (or was written to file while editing with aother program), it is not allowed to be edited
 
--- about opened files
-o.backup = False				-- Don't create backup file
-o.swapfile = false				-- do not  create swap file
-o.clipboard = "unnamed"		-- allow nvim to access system clipboard
-o.fileencoding = "utf-8"		-- encoding written to file
-o.undofile = true				-- enable persistent undo
+    -- about appearance
+    cmdheight = 2,                          -- more space in the neovim cmd line for displaying messages
+    showmode = true,                       -- we don't need to see -- INSERT -- anymore
+    showtabline = 2,                        -- always show tabs
+    cursorline = true,                      -- highlight the current line
+    number = true,                          -- show number in each line
+    relativenumber = false,                 -- set relative numbered lines
+    numberwidth = 4,                        -- set number column width to 4
+    signcolumn = "yes",                     -- always show sign column, otherwise it would shift the text weach time
+    pumheight = 10,                         -- pop up menu height
 
--- about appearance
-o.cmdheight = 2					-- more space in nvim cmd line for displaying messages
-o.cursorline = true				-- highlight line where the cursor is 
-o.number = true					-- show line numbers
-o.numberwidth = 4 				-- width of number column
+    -- about search
+    hlsearch = true,                        -- highlight all the matches on previous search pattern
+    ignorecase = true,                      -- ignore case in search pattern
+    incsearch = true,
 
--- mouse support & scroll
-o.scrolloff = 8
-o.mouse = "a"					-- enable mouse support in all NORMAL, VISUAL, INSERT & COMMAND-LINE mode
+    -- about mouse support
+    mouse = "a",                            -- allow mouse to be used in neovim
+    scrolloff = 5,                          -- 
 
--- about search
-o.hlsearch = true				-- higlight all matches
-o.incsearch = true				-- while typing, show where the search pattern is
-o.ignorecase = true				-- ignore casee in search pattern
-o.smartcase = true				-- override 'ignorecase' if search pattern contains uppercase
+    -- about typing
+    expandtab = true,                       -- convert tabs to spaces
+    shiftwidth = 4,                         -- the number of spaces inserted for each indentation
+    tabstop = 4,                            -- insert 4 spaces for each tab
+    smartcase = true,                       -- smart case
+    smartindent = true,                     -- make indenting smarter again
+    timeoutlen = 1000,                      -- time to wait for a mapped sequence to complete (in milliseconds)
+    updatetime = 300,                       -- faster completion (4000ms default)
 
--- about indentation
-o.smartindent = true
-o.autoindent = true
-o.smartindent = true			-- indent after a line ending with { or }
-o.backspace = {"indent", "eol", "start"}	-- allow backspacing over autoindent, eol & start of the insert
-o.expandtab = true				-- convert tabs to spaces
-o.shiftwidth = 4				-- no. of spaces inserted for each indentation
-o.tabstop = 4					-- no. of spaces to be inserted for eac tab
+    -- about splits
+    splitbelow = true,                      -- force all horizontal splits to go below the current window
+    splitright = true,                      -- force all vertical splits to go right of the current window
 
--- about splits
-o.splitbelow = true				-- force horizontal splits to go below current window
-o.splitright = true				-- force vertical sprints to go right current window
-o.timeoutlen = 300				-- 
+    completeopt = { "menuone", "noselect" },-- mostly just for cmp
+    -- termiguicolors = true,                   -- set term gui colors (most terminals support this)
+}
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+--vim.cmd [[set formatoptions-=cro]]
+
+vim.opt.shortmess:append "c"
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
