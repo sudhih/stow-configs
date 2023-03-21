25dbd88d1a3c8e193b8baa5d9f17d24d009a457b
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-03-22 03:28:41 +0530

    show only filename, instead of absolute path, in the 'lualine' status line

--- nvim/.config/nvim/lua/p-lualine.lua
+++ nvim/.config/nvim/lua/p-lualine.lua
@@ -1,10 +1,11 @@
 require('lualine').setup({
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { { 'filename', path = 3 } },
-        lualine_c = { 'diagnostics' },
-        lualine_x = {},
-        lualine_y = { 'location' },
-        lualine_z = { 'filetype' }
-    }
+  sections = {
+    lualine_a = { 'mode' },
+    -- show only filename, not the absolute path
+    lualine_b = { { 'filename', path = 0 } },
+    lualine_c = { 'diagnostics' },
+    lualine_x = {},
+    lualine_y = { 'location' },
+    lualine_z = { 'filetype' }
+  }
 })
