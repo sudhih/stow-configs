600f446815a8d0c38e7f96a59705c7059dd494cc
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-09-11 14:57:45 +0530

    `nnn` experiment, with initial 3 plugins enabled

--- zsh/nnn.zsh
+++ zsh/nnn.zsh
@@ -5,5 +5,11 @@
   # -r use advanced cp, mv patch
 export NNN_OPTS="er"
 
+# few plugins such `preview-tui` needs FIFO file
+export NNN_FIFO='/tmp/nnn.fifo'
+
 # key-plugin pairs
-export NNN_PLUG='f:finder;o:fzopen;j:autojump'
+export NNN_PLUG='o:fzopen;j:autojump;p:preview-tui'
+
+# set/export NNN_ICONLOOKUP as 1 to enable file icons in front of directory previews with .iconlookup
+export NNN_ICONLOOKUP=1
